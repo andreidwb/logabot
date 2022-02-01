07eecb515946f3330314763feabc81d166dca70e
andrei@dreamworkbros.com <andrei@dreamworkbros.com>
2022-02-01 21:54:02 +0100

    linux compatible environments

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,17 @@ import { bot } from "./bot/bot";
 import { appConfig } from "./config";
 
 const server = app.listen(appConfig.port || 3000, () => {
-    logger.info(`started server on http://localhost:${appConfig.port || 3000} in ${process.env.NODE_ENV} mode`);
+  logger.info(`started server on http://localhost:${appConfig.port || 3000} in ${process.env.NODE_ENV} mode`);
 });
 
 const httpTerminator = createHttpTerminator({ server });
 const shutdownSignals = ["SIGTERM", "SIGINT"];
 
 shutdownSignals.forEach((signal) =>
-    process.on(signal, async () => {
-        logger.info(`${signal} received, closing gracefully ...`);
-        await httpTerminator.terminate();
-    })
+  process.on(signal, async () => {
+    logger.info(`${signal} received, closing gracefully ...`);
+    await httpTerminator.terminate();
+  })
 );
 
 bot.launch();
